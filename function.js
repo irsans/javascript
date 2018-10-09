6f3f0cd57d9ad5c962bb3673a6c9176de4395525
irsans <taufikirsan1993@gmail.com>
2018-10-09 16:26:02 +0700

    menambahkan file function dan index

---
+++ function.js
@@ -0,0 +1,18 @@
+function jalanSatu() {
+  console.log("Jalan satu");
+}
+
+const jalandua = function()
+{
+  console.log("jalan dua");
+}
+
+jalanSatu()
+jalandua()
+
+const jalantiga=()=>
+{
+  console.log("jalan tiga");
+}
+
+jalantiga()
---
+++ index.js
@@ -0,0 +1,12 @@
+var message = "Hello coding";
+let msg1 = "Hello";
+const msg2 = "Coding";
+
+if (true) {
+  var message = 1;
+  let msg1 = 2;
+  console.log("testing",msg1);
+  console.log(message)
+}
+console.log(message)
+console.log(msg1)
