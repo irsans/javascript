791af25831644fa51d54028ce130620ff01c1fe0
irsans <taufikirsan1993@gmail.com>
2019-03-04 16:47:18 +0700

    menambahkan arraysplit.js

---
+++ array-split.js
@@ -0,0 +1,8 @@
+function panggilsplit() {
+  var kalimat = "kita sedang belajar javascript";
+  console.log(kalimat);
+  var result = kalimat.split(" ");
+  return result
+}
+
+console.log(panggilsplit());
